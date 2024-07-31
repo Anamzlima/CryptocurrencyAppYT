dd68a67c8fcad45b74871f51952f23d6519eff68
ana.lima <ana.lima@thoughtworks.com>
2024-07-30 23:07:03 -0300

    Add interceptor and additional permission in manifest

--- app/src/main/java/com/plcoding/cryptocurrencyappyt/di/AppModule.kt
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/di/AppModule.kt
@@ -8,6 +8,9 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -15,12 +18,28 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+    @Provides
+    @Singleton
+    fun providesHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY // Logs body content
+        return loggingInterceptor
+    }
+
+    @Provides
+    @Singleton
+    fun providesOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(providesHttpLoggingInterceptor())
+            .build()
+    }
 
     @Provides
     @Singleton
     fun providesPaprikaApi(): CoinPaprikaApi {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(providesOkHttpClient())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(CoinPaprikaApi::class.java)
